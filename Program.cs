5f6c7d35d83596e79a651f6b15b0daf80f336287
Joseph M. Myers <joecaricature@gmail.com>
2021-01-08 04:55:38 -0500

    Hope I'm on the right track

--- Program.cs
+++ Program.cs
@@ -2,11 +2,14 @@
 
 namespace VariablesCS
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            int numberOfCupsOfCoffee = 2;
+            string fullName = "Joseph Myers";
+            var today = DateTime.Today;
+            Console.WriteLine("Welcome to C#");
+        }
     }
-  }
 }
