29d7e17348870c3c332c46727f5da35ac235dc3b
Joseph M. Myers <joecaricature@gmail.com>
2021-01-10 18:55:13 -0500

    Fixed Readline issue

--- Program.cs
+++ Program.cs
@@ -15,21 +15,20 @@ namespace VariablesCS
 
             Console.WriteLine("{0} {1} {2} {3}", numberOfCupsOfCoffee, dailycups, fullName, today);
 
-            while (!good)// so while the true/false variable is true. The double-negatives are making my brain hurt.
-            {
-                // I am piecing this together from things I found online. 
-                Console.WriteLine("What's your name?:");
-                input = Console.ReadLine();
-                good = !string.IsNullOrEmpty(input); //I think the '!' is denoting the opposite of true/false. I don't quite understand what is modifies before 'string'.
-                if (!good)
-                    Console.WriteLine("Please tell me your name!");
-            }
-            while (good) // i think this is checking to see if the user has entered something.
-            {
-                good = string.IsNullOrEmpty(input);
-                if (good)
-                    Console.WriteLine("Hello!,(input)");//I'm trying to check if the user has entered an input- and if they have- print a greeting with their input included.
-            }
+
+            // I am piecing this together from things I found online. 
+            Console.WriteLine("What's your name?:");
+            input = Console.ReadLine();
+            good = !string.IsNullOrEmpty(input); //I think the '!' is denoting the opposite of true/false. I don't quite understand what is modifies before 'string'.
+            if (!good)
+                Console.WriteLine("Please tell me your name!");
+            if (good)
+                Console.WriteLine($"Hello, {input}!");
+
+
+
+
+
         }
     }
 }
